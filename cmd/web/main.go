d1964f5aea69706f81eaab7ec1e1e409eb382475
w0 <33212583+w0@users.noreply.github.com>
2026-03-31 22:17:41 -0500

    open sqlite db

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	"github.com/w0/tinyIPAM/internal/database"
+	_ "modernc.org/sqlite"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	database *database.Queries
 }
 
 func main() {
 
 	addr := flag.String("addr", ":8080", "HTTP network address")
+	dsn := flag.String("dsn", "file:ipam.db?&_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)", "database connection string")
 
 	flag.Parse()
 
@@ -21,14 +27,35 @@ func main() {
 		AddSource: true,
 	}))
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		database: database.New(db),
 	}
 
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
