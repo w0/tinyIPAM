ebdb43a042ec313a6511df5b883691d0cc3a3e35
w0 <33212583+w0@users.noreply.github.com>
2026-04-02 21:56:31 -0500

    add handlers for subnet routes

---
+++ cmd/web/handlers_subnets.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/w0/tinyIPAM/internal/database"
+)
+
+type subnet struct {
+	Id            int    `json:"id,omitempty"`
+	Name          string `json:"name"`
+	NetworkPrefix string `json:"network_prefix"`
+	Cidr          int    `json:"cidr"`
+}
+
+func (app *application) newSubnet(w http.ResponseWriter, r *http.Request) {
+
+	decoder := json.NewDecoder(r.Body)
+	var s subnet
+	if err := decoder.Decode(&s); err != nil {
+		app.logger.Debug("failed to decode subnet", "error", err)
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	err := app.database.NewSubnet(r.Context(), database.NewSubnetParams{
+		Name:          s.Name,
+		NetworkPrefix: s.NetworkPrefix,
+		Cidr:          int64(s.Cidr),
+	})
+
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+}
+
+func (app *application) getSubnet(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	if id == "" {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	intId, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	s, err := app.database.GetSubnet(r.Context(), intId)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	json.NewEncoder(w).Encode(s)
+}
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,6 +6,8 @@ func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("POST /api/v1/subnet", app.newSubnet)
+	mux.HandleFunc("GET /api/v1/subnet/{id}", app.getSubnet)
 
 	return mux
 }
